11d3ca28de3323545f56b0acdd333e0b54edf521
MACBOOK <macbook@n158-p218.kthopen.kth.se>
2016-09-09 12:18:08 +0200

    jag lade till denna kommentar

--- F2/F2.hs
+++ F2/F2.hs
@@ -2,42 +2,44 @@
 -- Ousainou Manneh & Thony Price  - Cdate2
 
 module F2 where
-  
+
 -- 2.   Molekylära sekvenser
--- 2.1  Skapa datatypen MolSeq för molekylära sekvenser som anger 
---      sekvensnamn, sekvens(ensträng), och om det är DNA eller 
+-- 2.1  Skapa datatypen MolSeq för molekylära sekvenser som anger
+--      sekvensnamn, sekvens(ensträng), och om det är DNA eller
 --      protein som sekvensen beskriver
 
 data Molseq = DNA [Char] [Char] | Protein [Char] [Char] deriving (Show)
 
--- 2.2  Skriv en funktion string2seq med typsignaturen 
---      String -> String -> MolSeq.Dess första argument är ett namn och 
---      andra argument är en sekvens. Denna funktion ska automatiskt skilja 
---      på DNA och protein, genom att kontrollera om en sekvens bara 
+-- 2.2  Skriv en funktion string2seq med typsignaturen
+--      String -> String -> MolSeq.Dess första argument är ett namn och
+--      andra argument är en sekvens. Denna funktion ska automatiskt skilja
+--      på DNA och protein, genom att kontrollera om en sekvens bara
 --      innehåller A, C, G, samt T och då utgå ifrån att det är DNA.
 
 string2seq :: String -> String -> Molseq
 -- Första argumentet är ett namn, andra är en sekvens
 string2seq n []   = Protein n []
 string2seq n (x:xs)
-  | [x] `elem` letters = string2seq n (xs)  
+  | [x] `elem` letters = string2seq n (xs)
   | otherwise = DNA n xs
   where letters = ["A", "C", "G", "T"]
+
+-- we are the best
 
 
 
 -- 2.3  Skriv tre funktioner seqName, seqSequence, seqLength som tar en
---      MolSeq och returnerar namn, sekvens, respektive sekvenslängd. Du ska 
+--      MolSeq och returnerar namn, sekvens, respektive sekvenslängd. Du ska
 --      inte behöva duplicera din kod beroende på om det är DNA eller protein!
 
 seqName :: Molseq -> IO()
 seqName (DNA a b) = putStrLn b
 seqName (Protein a b) = putStrLn b
--- 
+--
 -- seqSequence :: Molseq -> [Char]
 -- -- Retunerar sekvensen
 -- seqSequence n s = s
--- 
+--
 -- seqLength :: Molseq -> Int
 -- -- Retunerar sekvensens längd OBS! Kan snabbas upp, length = långsam
 -- seqLength n s = length s
