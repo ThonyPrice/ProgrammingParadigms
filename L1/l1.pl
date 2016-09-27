e415bcb04bd64adfcf695945b63cbdc4b966f087
Thony Price <Thony@Thonys-MacBook-Pro.local>
2016-09-27 19:08:00 +0200

    Skelett för rovarsprak, fungerar dock ej

--- L1/l1.pl
+++ L1/l1.pl
@@ -1,18 +1,15 @@
 % Programmeringsparadigm (DD1361) HT16 - L1
 % Thony Price & Ousainou Manneh, CDATE2
 
-% 1.  Fibonacci-talen
-%     Skriv ett predikat fib(+N, ?F) som är sant om och endast om F är det 
-%     N’te Fibonacci-talet. Målet fib(30, F) ska alltså ha exakt en lösning,
-%     nämligen att F är det trettionde Fibonacci-talet.
+% -*-    1. Fibonacci-talen    -*- %
 
-% Base predicate for F = 0
+% Basfall för F = 0
 fib(0, 0).
-% Base predicate for F = 1
+% Basfall för F = 1
 fib(1, 1).
 
-% Recursive function
-fib(+N, ?F) :-
+% Rekursiv funktion (lägg till mer kommentarer)
+fib(N, F) :-
   N > 1,
   N2 is N-2, fib(N2, F2),
   N1 is N-1, fib(N1, F1),
@@ -22,3 +19,42 @@ fib(+N, ?F) :-
 % 1.  Uppgiftsbeskrivningen använder metasymboler som +N 
 %     och ?F - Vad betyder dem och hur ska de implementeras?
 % 2.  Funktionen klarar inte av fib(30, F) - Hur optimera?
+
+
+% -*-    2. Rovarsprak    -*- %
+
+% Funktion för att se om en bokstav är en vokal
+vokal(X) :- member(X, [101, 105, 111, 117, 125, 131]).
+
+% Basfall för tom lista (när alla bokstäverna gåtts igenom)
+rovarsprak([], []).
+
+% Rekursiv funktion som fångar fall med vokal som head
+rovarsprak(Text, RovarText) :-
+  write("Hej"),
+  Text \= [],
+  [H|T] = Text,
+  vokal(H),
+  write(H),
+  append([H], rovarsprak(T, RovarText), X),
+  RovarText is X.
+
+
+% -*-    3. Medellangd    -*- %
+
+% Basfall för medellangd
+% medellangd([])
+% 
+% medellangd(Text, AvgLen) :-
+  
+  
+
+
+
+
+
+% Givet kodskelett;
+% fib(N, F) :- F = N.
+% rovarsprak(Text, RovarText) :- RovarText = Text.
+% medellangd(Text, AvgLen) :- AvgLen = 1.0.
+% skyffla(Lista, Skyfflad) :- Skyfflad = Lista.
