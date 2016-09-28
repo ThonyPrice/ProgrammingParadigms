f0c76cd14d93852207ca296c73d4c8aa8efa6759
Thony Price <Thony@n130-p58.kthopen.kth.se>
2016-09-28 14:34:57 +0200

    Uppgift 3 verkar fungera korrekt

--- L1/l1.pl
+++ L1/l1.pl
@@ -42,17 +42,49 @@ rovarsprak(Text, RovarText) :-
 
 % -*-    3. Medellangd    -*- %
 
-% Basfall för medellangd
-% medellangd([])
-% 
-% medellangd(Text, AvgLen) :-
+% Om teckenkod är bokstav
+enbokstav(Decimal) :-
+  char_code(X, Decimal),
+  is_alpha(X).
+
+% Unifiera ackumulerade summan med Result när listan gåtts igenom
+sum_bokstaver([], Ack, Ack).
+
+% Summera all bokstäver genom att öka ackumulator vid funnen bokstav
+sum_bokstaver([H|T], Ack, Result) :-
+  enbokstav(H),
+  NyAck is Ack + 1,
+  sum_bokstaver(T, NyAck, Result);
+  not(enbokstav(H)),
+  sum_bokstaver(T, Ack, Result).
   
+% Basfall som binder ackumulerade summan till Result
+sum_ord([], Ack, Ack).
+
+% Om en bokstav upptäcks -> Ord påbörjat -> Öka ackumulatorn
+% och skicka till tills_ejord som loopar tills nytt ord påbörjas.
+sum_ord([H|T], Ack, Result) :-
+  enbokstav(H),
+  NyAck is Ack + 1,
+  tills_ejord(T, NyAck, Result);
+  not(enbokstav(H)),
+  sum_ord(T, Ack, Result).
+
+% Se kommentar över sum_ord.
+tills_ejord([], Ack, Ack).
+tills_ejord([H|T], Ack, Result) :-
+  enbokstav(H),
+  tills_ejord(T, Ack, Result);
+  not(enbokstav(H)),
+  sum_ord(T, Ack, Result).
   
-
-
-
-
-
+% Huvudfunktion: dividerar antal bokstäver med antal ord och
+% unifierar svaret med AvgLen
+medellangd(Text, AvgLen) :-
+   sum_bokstaver(Text, 0, X),
+   sum_ord(Text, 0, Y),
+   AvgLen is (X/Y).
+  
 % Givet kodskelett;
 % fib(N, F) :- F = N.
 % rovarsprak(Text, RovarText) :- RovarText = Text.
