31b8e015c6c6970635eae3e53b71657fab1decd9
Thony Price <Thony@n130-p58.kthopen.kth.se>
2016-09-29 10:15:49 +0200

    Final version

--- L1/l1.pl
+++ L1/l1.pl
@@ -1,14 +1,15 @@
-% Programmeringsparadigm (DD1361) HT16 - L1
-% Thony Price & Ousainou Manneh, CDATE2
+% Laboration 1 Prolog- uppvärmning
+% Logikprogrammering
+% Thony Price, Ousainou Manneh
 
-% -*-    1. Fibonacci-talen    -*- %
+% Uppgift 1
 
-% Beräkna det N:e fibonaccitalet 
+% Beräkna det N:e fibonaccitalet
 fib(N, F) :-
   N > 0,
   fib_help(N,0,1,F).
-  
-% När rekursionen i fib_help nått fib-talet 1, 
+
+% När rekursionen i fib_help nått fib-talet 1,
 % unifiera det ackumulerade fibtalet med F
 fib_help(1,_,F,F).
 
@@ -19,25 +20,27 @@ fib_help(N,A1,A2,F) :-
   N1 is N-1,
   Ack is A1+A2,
   fib_help(N1,A2,Ack,F).
-  
-% -*-    2. Rovarsprak    -*- %
 
-% Funktion för att se om en bokstav är en vokal
-vokal(X) :- member(X, [101, 105, 111, 117, 125, 131]).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Basfall för tom lista (när alla bokstäverna gåtts igenom)
+% Rövarspråk Uppgift 2
+
 rovarsprak([], []).
 
-% Rekursiv funktion som fångar fall med vokal som head
-rovarsprak(Text, RovarText) :-
-  write("Hej"),
-  Text \= [],
-  [H|T] = Text,
-  vokal(H),
-  write(H),
-  append([H], rovarsprak(T, RovarText), X),
-  RovarText is X.
+% Här mönstermatchas head för båda två
+rovarsprak([H|T], [H|T2]) :-
+% Här kollar vi ifall det är en vokal
+member(H, [97, 101, 105, 111, 117, 121]),
+% Rekursivt anrop
+rovarsprak(T, T2).
 
+% Här mönstermnatchas de båda headen igen, men lägger till ett o mellan de.
+rovarsprak([H|T], [H,111,H|T2]) :-
+rovarsprak(T, T2).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Medellängd Uppgift 3
 
 % -*-    3. Medellangd    -*- %
 
@@ -56,7 +59,7 @@ sum_bokstaver([H|T], Ack, Result) :-
   sum_bokstaver(T, NyAck, Result);
   not(enbokstav(H)),
   sum_bokstaver(T, Ack, Result).
-  
+
 % Basfall som binder ackumulerade summan till Result
 sum_ord([], Ack, Ack).
 
@@ -76,16 +79,42 @@ tills_ejord([H|T], Ack, Result) :-
   tills_ejord(T, Ack, Result);
   not(enbokstav(H)),
   sum_ord(T, Ack, Result).
-  
+
 % Huvudfunktion: dividerar antal bokstäver med antal ord och
 % unifierar svaret med AvgLen
 medellangd(Text, AvgLen) :-
    sum_bokstaver(Text, 0, X),
    sum_ord(Text, 0, Y),
    AvgLen is (X/Y).
-  
-% Givet kodskelett;
-% fib(N, F) :- F = N.
-% rovarsprak(Text, RovarText) :- RovarText = Text.
-% medellangd(Text, AvgLen) :- AvgLen = 1.0.
-% skyffla(Lista, Skyfflad) :- Skyfflad = Lista.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% Uppgift 4
+
+%skyffla(lista, skyfflad)
+
+% Här blir det sant med två listor
+skyffla([], []).
+skyffla([H|[]], [H|[]]).
+
+% Här skyfflas listan med två hjälpfunktioner
+% en som somskyfflar och den andra som förberedder nästa skyffel med resten av listan
+skyffla([H,N | T], Skyfflad) :-
+skyff([H, N |T], X),
+rest([H, N|T], Rest1),
+skyffla(Rest1, Y),
+append(X, Y, Skyfflad).
+
+%Här skyfflas det, returnerar varannat element
+skyff([], []).
+skyff([H| []], [H|[]]).
+
+skyff([H, _|T], [H|T2]):-
+skyff(T, T2).
+
+% Tar varannat element för att förberedda nästa skyffling
+rest([], []).
+rest([_| []], []).
+rest([_, N|T], [N|T2]):-
+rest(T, T2).
