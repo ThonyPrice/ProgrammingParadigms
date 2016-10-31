b546876270df48ffc7e00391a3a226fe11503588
Thony Price <Thony@Thonys-MacBook-Pro.local>
2016-10-31 21:28:59 +0100

    First 4 exercises solved

--- S1/s1.py
+++ S1/s1.py
@@ -1,4 +1,34 @@
-# Project:            S1 - Programmeringsparadigm
-# Authors:            Thony Price 
-# Last revision:      2016-10-31
+# Project:              S1 - Programmeringsparadigm
+# Authors:              Thony Price 
+# Last revision:        2016-10-31
 
+#!/usr/bin/python
+import re
+
+def dna():              # Uppgift 1
+    return "^[ACGT]+$"
+    
+def sorted():           # Uppgift 2
+    return "^9*8*7*6*5*4*3*2*1*0*$"
+    
+def hidden1(x):         # Uppgift 3
+    # Indata x är strängen som vi vill konstruera ett regex för att söka efter
+    return x
+    
+def hidden2(x):         # Uppgift 4
+    # Indata x är strängen som vi vill konstruera ett regex för att söka efter
+    # Tanke: Om x är "progp" kan regex vara: .*p.*r.*o.*g.*p.*
+    r = ""
+    for letter in x:
+        r += ".*" + str(letter)
+    r += ".*"
+    return r
+
+def equation():     # uppgift 5
+    return ""
+
+def parentheses():  # uppgift 6
+    return ""
+
+def sorted3():      # uppgift 7
+    return ""
