f6b990ea46631c2b03d23522516da5d6886dc502
Thony Price <Thony@n157-p30.kthopen.kth.se>
2016-11-10 15:27:32 +0100

    Added possible grammatic

--- S2/s2.py
+++ S2/s2.py
@@ -1,3 +1,15 @@
 # Programmeringparadigm   Lab 10 
 # Created by:             Thony Price 
 # Last revision:          2016-11-10
+
+
+
+def main():
+    userInput = sys.stdin.readlines()
+    for line in userInput:
+        pass
+    # -- END -- #
+
+# Runs main program from this module
+if __name__ == "__main__":
+    main ()
